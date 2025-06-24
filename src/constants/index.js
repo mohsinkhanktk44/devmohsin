e401c51c922917ffb5dce3b3f33fa529954ed44e
Mohsin <mohsinkhanktk44@gmail.com>
2025-06-25 04:03:07 +0500

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,497 @@
+
+import {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  express,
+  aws,
+  mui,
+  shadcn,
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  rohtas,
+  focusit,
+  marhaba,
+  alimcosoft,
+  maxoprta,
+  admintracker,
+  foodil,
+  projectneeca,
+  polarpro,
+  hearingly,
+  PMPUUkraine,
+  accountDashboard,
+  FuzzieDashboard,
+  arrowlink,
+} from '../assets'
+
+export const navLinks = [
+
+
+  {
+    id: "about",
+    title: "About",
+    
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Ui UX Designer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "gsap",
+    icon: gsap,
+  },
+  {
+    name: "framer",
+    icon: framer,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Material Ui",
+    icon: mui,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Express Js",
+    icon: express,
+  },
+  {
+    name: "Shad cn",
+    icon: shadcn,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "MySql",
+  //   icon: mysql,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+
+];
+
+const experiences = [
+  {
+    title: "Front-End Developer",
+    company_name: "Marahba group of companies",
+    icon: marhaba,
+    iconBg: "#383E56",
+    date: "MAY 2023 - JAN 2025",
+    points: [
+      "Worked as a Front-End Developer at Marhaba Group of Companies, creating responsive and user-friendly web interfaces using modern JavaScript frameworks",
+      "Gained hands-on experience with Node.js and Express.js, contributing to the development of backend APIs and server-side logic",
+     "Proactively learned new technologies and contributed to both front-end and back-end tasks demonstrating versatility and a growth mindset."
+
+    ],
+  },
+  {
+    title: "Front-End Developer",
+    company_name: "Focus IT Services",
+    icon: focusit,
+    iconBg: "#383E56",
+    date: "Feb 2022 - March 2023",
+    points: [
+      "Translated design concepts into responsive and visually compelling user interfaces for web applications",
+      "Ensured a seamless and intuitive user experience across various devices and screen sizes",
+      "Collaborated closely with cross-functional teams to align design and technical requirements.",
+      "Delivered high-quality, user-centric web applications that met project goals and standards",
+    ],
+  },
+  {
+    title: "Front-End Developer",
+    company_name: "Alimcosoft Pvt. Limited",
+    icon: alimcosoft,
+    iconBg: "#383E56",
+    date: "Jan 2021 - Dec 2021",
+    points: [
+      "Specialized in creating sleek, visually appealing user interfaces using Tailwind CSS",
+      "Demonstrated strong proficiency in integrating REST APIs for efficient data interaction within applications.",
+      "Applied expertise in modern front-end technologies to deliver high-quality, responsive designs.",
+      "Focused on enhancing user engagement and improving overall application performance.",
+    ],
+  },
+   {
+    title: "Front-End Developer",
+    company_name: "Rohtas Marketing",
+    icon: rohtas,
+    iconBg: "#383E56",
+    date: "july 2019 - Nov 2020",
+    points: [
+      "Built and updated landing pages and dashboards using HTML, CSS, JavaScript, and React.js.",
+      "Integrated third-party APIs to fetch and display property listings dynamically",
+      "Worked closely with the marketing team to implement SEO-friendly and mobile-responsive designs.",
+      "Created reusable components to speed up development and maintain code consistency",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Working with Mohsin Khan was exceptional! His attention to detail truly elevated the quality of the project. He communicated with my team and demonstrated a stellar understanding of styled components! Well-deserved!",
+    name: "Jill",
+    designation: "United States",
+    company: "Fiver",
+    // image: firstTestimonial,
+  },
+  {
+    testimonial:
+      "This designer is simply AWESOME. He is very fast & very adaptable to what we need. I will not use anyone else for my web design work as I am very happy with him. His pricing is very affordable & fair. I highly recommend him.",
+    name: "Daniel",
+    designation: "Indonesia",
+    company: "Fiver",
+    // image: secondTestimonial,
+  },
+  {
+    testimonial:
+      "Great service. Fast turnaround. very skilled and understandable Easy to work with. Thanks so much for your help with my site.",
+    name: "James Wang",
+    designation: "Germany",
+    company: "Fiver",
+    // image: thirdTestimonial,
+  },
+];
+
+const projects = [
+  {
+    name: "Project Premium Deliveries",
+    description:
+      "The delivery management platform simplifies the entire delivery process with real-time tracking, smart driver assignment, and optimized routing, Its intuitive map interface enhances order entry and location selection, ensuring speed and transparency.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "white-text-gradient",
+      },
+      {
+        name: "Redux Toolkit",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Leaflet",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: maxoprta,
+    live_demo_link: "https://piloting.premierdeliveries.co.uk/",
+    // source_code_link: "https://github.com/",
+  },
+  {
+    name: "Project Tracker- Employee",
+    description:
+      "A versatile time tracking system with dedicated modules for admins, employees, and managers. Admins manage users, track time, exchange documents, generate salary slips, and post announcements.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "white-text-gradient",
+      },
+      {
+        name: "Redux Toolkit",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "GraphQL",
+        color: "green-text-gradient",
+      },
+       {
+        name: "storybook",
+        color: "blue-text-gradient",
+      },
+      
+    ],
+    image: admintracker,
+    source_code_link: "https://github.com/mohsinkhanktk44/Tracker-for-employees",
+  },
+  {
+    name: "Project Foodil - B2B food export trading platform",
+    description:
+      "Foodil is a B2B food export trading platform that connects Korean food manufacturers with global buyers. It streamlines product listing, ordering, logistics, and payments, making it easy for businesses to trade Korean food products worldwide. It's designed for wholesale and international trade, not individual consumers.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "GraphQL",
+        color: "white-text-gradient",
+      },
+      {
+        name: "storybook",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: foodil,
+    // source_code_link: "https://github.com/",
+    live_demo_link: "https://foodil.com/",
+  },
+  {
+    name: "Project Neeca",
+    description:
+      "As a front-end developer, I worked on Neeca, designing and implementing components for roles like Adminand Vendor. I ensured a smooth user experience and integrated frontend elements with backend functionalities.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "GraphQL",
+        color: "white-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: projectneeca,
+    // source_code_link: "https://github.com/",
+    live_demo_link: "https://forms.neeca.pk/login",
+
+  },
+   {
+    name: "Project PolarPro",
+    description:
+      "As a Frontend Developer at PolarPro, I successfully completed a few tasks assigned by management, including API integrations and designing and developing components and pages.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "GraphQL",
+        color: "white-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: polarpro,
+    // source_code_link: "https://github.com/",
+    live_demo_link: "https://www.polarpro.com/",
+
+  },
+   {
+    name: "Project hearingly",
+    description:
+      "As a Frontend Developer at Hearingly, I integrated APIs and designed components using Next.js, Tailwind CSS, and TypeScript. My work enhanced performance and user experience on the platform.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "GraphQL",
+        color: "white-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: hearingly,
+    // source_code_link: "https://github.com/",
+    live_demo_link: "https://www.hearingly.com/",
+
+  },
+   {
+    name: "Project PMPU-Ukraine",
+    description:
+      " PMPUKRAINE a user management dashboard designed for municipalities and organizations to manage, evaluate, and track refurbishment and modernization projects for public buildings, including proposals, applications, and related documentation.",
+    tags: [
+      {
+        name: "React js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "React Query",
+        color: "white-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: PMPUUkraine,
+    source_code_link: "https://github.com/mohsinkhanktk44/user-Management-dashboard",
+  },
+   {
+    name: "Project User Management",
+    description:
+      "Developed a responsive web-based accounting dashboard using React and Chakra UI, enabling users to manage invoices and payment methods efficiently. Integrated billing analytics and a modern UI for streamlined financial management",
+    tags: [
+      {
+        name: "React js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Chakra UI",
+        color: "white-text-gradient",
+      },
+    ],
+    image: accountDashboard,
+    source_code_link: "https://github.com/mohsinkhanktk44/AccountDashboard",
+  },
+    {
+    name: "Project Fuzzie Dashboard",
+    description:
+      "Fuzzie Dashboard is a modern Next.js application featuring real-time collaboration, user authentication, and interactive drag-and-drop interfaces. It integrates with Stripe for payments, Notion for data, and supports advanced UI components, theming, and analytics for a seamless dashboard experience.",
+    tags: [
+      {
+        name: "Next js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux Toolkit",
+        color: "white-text-gradient",
+      },
+      {
+        name: "Tailwind css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Typescript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: FuzzieDashboard,
+    source_code_link: "https://github.com/mohsinkhanktk44/fuzzie_dashbord",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
